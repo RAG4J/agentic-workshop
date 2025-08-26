6308559700dc247d424b5026d8524e037712f004
Jettro Coenradie <jettro@coenradie.com>
2025-08-26 10:40:55 +0200

    Move to yml for configuration, adds MCP

--- springai-agent/src/main/java/org/rag4j/agent/springai/SpringAIAgentConfig.java
+++ springai-agent/src/main/java/org/rag4j/agent/springai/SpringAIAgentConfig.java
@@ -1,14 +1,18 @@
 package org.rag4j.agent.springai;
 
+import io.modelcontextprotocol.client.McpSyncClient;
 import org.rag4j.agent.core.Agent;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.mcp.SyncMcpToolCallbackProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import java.util.List;
 
 @Configuration
 @Profile("springai")
@@ -20,9 +24,10 @@ public class SpringAIAgentConfig {
     }
 
     @Bean
-    public ChatClient chatClient(ChatModel chatModel,  ChatMemory chatMemory) {
+    public ChatClient chatClient(ChatModel chatModel, ChatMemory chatMemory, List<McpSyncClient> mcpSyncClients) {
         return ChatClient.builder(chatModel)
                 .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
+                .defaultToolCallbacks(new SyncMcpToolCallbackProvider(mcpSyncClients))
                 .build();
     }
 
